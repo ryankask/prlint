0d124759291136765fb306e5d06ce70db3503add
James Cooke <jamescooke@users.noreply.github.com>
2016-09-28 10:04:04 +0100

    Process ping payloads at GitHub webhook endpoint

--- prlint/github/views.py
+++ prlint/github/views.py
@@ -1,16 +1,48 @@
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
 from rest_framework.views import APIView
+
+from .serializers import (
+    HeaderSerializer,
+    HookPayloadSerializer,
+    PingPayloadSerializer,
+    RepositoryPayloadSerializer,
+)
 
 
 class GitHubView(APIView):
 
     def post(self, request):
         """
-        Process pull_request and ping webhook payloads from GitHub.
+        Process ping webhook payloads from GitHub.
+
+        Order of processing:
+
+        * Validate that repository is registered. If not 401 Unauthorized.
+        * Validate passed event. If not 403 Forbidden.
+        * Pass request to `pull_request` or `ping` handlers.
+
+        # TODO
+
+        * Process pull_request payloads.
         """
-        # TODO fix URL to be generated dynamically
-        data = {
-            'detail': 'Repository is not registered with prlint. Please visit https://prlint.com/ to register.'
-        }
-        return Response(data, status=status.HTTP_403_FORBIDDEN)
+        repo_serializer = RepositoryPayloadSerializer(data=request.data)
+        try:
+            repo_serializer.is_valid(raise_exception=True)
+        except ValidationError:
+            return Response(repo_serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+
+        header_serializer = HeaderSerializer(request)
+        try:
+            header_serializer.is_valid(raise_exception=True)
+        except ValidationError:
+            return Response(header_serializer.errors, status=status.HTTP_403_FORBIDDEN)
+
+        ping_serializer = PingPayloadSerializer(data=request.data)
+        try:
+            ping_serializer.is_valid(raise_exception=True)
+        except ValidationError:
+            return Response(ping_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({}, status=status.HTTP_200_OK)
