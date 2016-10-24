a7526f739697f314c24523ea2a42f700033a113e
James Cooke <jamescooke@users.noreply.github.com>
2016-10-24 08:46:49 +0100

    Add failing test for receiving pull request

--- prlint/github/tests/views/test_post.py
+++ prlint/github/tests/views/test_post.py
@@ -5,7 +5,11 @@ from rest_framework.test import APITestCase
 from ...factories import RepositoryFactory
 from ...models import Repository
 from ...views import GitHubView
-from ..payload_factories import PayloadRequestFactory, PingEventFactory
+from ..payload_factories import (
+    PayloadRequestFactory,
+    PingEventFactory,
+    PullRequestEventFactory,
+)
 
 
 class TestPost(APITestCase):
@@ -37,6 +41,18 @@ class TestPostUnregistered(TestPost):
         GitHub Ping webhook returns 401 UNAUTH when no matching Repository
         """
         request = PingEventFactory()
+
+        result = GitHubView.as_view()(request)
+
+        self.assertEqual(result.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertIn('repository', result.data)
+        self.assertIn('id', result.data['repository'])
+
+    def test_pull_request_no_repo(self):
+        """
+        GitHub webhook returns
+        """
+        request = PullRequestEventFactory()
 
         result = GitHubView.as_view()(request)
 
@@ -85,6 +101,22 @@ class TestPostRegistered(TestPost):
         """
         request = PingEventFactory(
             hook__events=['*'],
+            repository__id=123456,
+        )
+
+        result = GitHubView.as_view()(request)
+
+        self.assertEqual(result.status_code, 400)
+        self.assertEqual(list(result.data['hook']), ['events'])
+        self.assertIn('Events received were "[\'*\']"', result.data['hook']['events'][0])
+
+    # PULL REQUEST
+
+    def test_pull_request(self):
+        """
+        GitHub webhook returns ?
+        """
+        request = PullRequestEventFactory(
             repository__id=123456,
         )
 
